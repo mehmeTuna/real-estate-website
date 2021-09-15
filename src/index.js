f515d32be317eb1703b5627704042091aebfa5a1
Mehmet Tuna <mehmet.tuna@tsoft.com.tr>
2021-09-15 20:40:47 +0300

    Created web app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'normalize.css';
+
 import './index.css';
 import App from './App';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
